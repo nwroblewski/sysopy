e170dd3b2f58006746d7af52ace9d68627196710
nwroblewski <mikolaj.wroblewski1997@gmail.com>
2018-03-27 23:38:12 +0200

    zmniejszylem troszke rozmiar alokowanej pamieci, alokowanie jednego megabajta srednio na takt to chyba troszke za duzo bylo jednak

--- zestaw3/true.c
+++ zestaw3/true.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]){
 
     int i = 0; 
     while(1){
-        char * alotof = malloc(sizeof(char)* 1024 * 1024);
+        char * alotof = malloc(sizeof(char)* 1024);
     }
 
 }
